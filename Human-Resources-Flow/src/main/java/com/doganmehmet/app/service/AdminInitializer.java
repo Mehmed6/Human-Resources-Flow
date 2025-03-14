8e02bd7050cc9b3d44dd88351846e74e5422420a
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-03-14 05:35:54 +0100

    Automatically create an admin user on application startup
    Implemented functionality to automatically create an admin user upon application startup, with the following credentials:
    - Username: admin
    - Password: admin
    - Admin user is created if not already present in the database

---
+++ Human-Resources-Flow/src/main/java/com/doganmehmet/app/service/AdminInitializer.java
@@ -0,0 +1,41 @@
+package com.doganmehmet.app.service;
+
+import com.doganmehmet.app.entity.Admin;
+import com.doganmehmet.app.enums.Role;
+import com.doganmehmet.app.repository.IUserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AdminInitializer implements CommandLineRunner {
+
+    private final IUserRepository m_userRepository;
+    private final BCryptPasswordEncoder m_bCryptPasswordEncoder;
+
+    public AdminInitializer(IUserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder)
+    {
+        m_userRepository = userRepository;
+        m_bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public void run(String... args)
+    {
+        if (!m_userRepository.existsByRole(Role.ADMIN)) {
+            var admin = new Admin();
+            admin.setUsername("admin");
+            admin.setPassword(m_bCryptPasswordEncoder.encode("admin"));
+            admin.setEmail("admin@gmail.com");
+            admin.setFirstName("Admin");
+            admin.setLastName("Admin");
+            admin.setRole(Role.ADMIN);
+            admin.setPhone("00000000000");
+            m_userRepository.save(admin);
+
+            System.out.printf("Admin has been created. Username:%s password:%s", admin.getUsername(), admin.getPassword());
+
+        }
+    }
+}
